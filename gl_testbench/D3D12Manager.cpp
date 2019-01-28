317cf9e8e04f22f7de33f4e4aed264fd90d397fd
Oliver <oliverglandberger@gmail.com>
2019-01-28 15:23:03 +0100

    Saving progress before merging the newest changes from master into this branch.

---
+++ gl_testbench/D3D12Manager.cpp
@@ -0,0 +1,46 @@
+#include "D3D12Manager.h"
+#include <d3d12.h>
+
+D3D12Manager::D3D12Manager()
+{
+}
+
+D3D12Manager::~D3D12Manager()
+{
+}
+
+void D3D12Manager::initialize()
+{
+}
+
+void D3D12Manager::render()
+{
+	// Populate the command list
+	// Execute the command list
+	// IDXGISwapChain1::Present1
+	// Wait for the GPU to finish
+
+	// Create VS
+	// Create PS
+	// Create PSO
+	
+	
+	// --------------------------------
+	// CODE-OBSERVED ------------------
+	// --------------------------------
+
+	/// Record all the commands we need to render the scene into the command list.
+	// PopulateCommandList();
+
+	/// Execute the command list.
+	ID3D12
+
+}
+
+void D3D12Manager::update()
+{
+}
+
+void D3D12Manager::destroy()
+{
+}
