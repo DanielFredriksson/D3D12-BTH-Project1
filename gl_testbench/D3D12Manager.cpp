9c840227c51e2f50ce4bee3ecb9ab2d8ea9ec369
Daniel Fredriksson <daniel.ls.fredriksson@gmail.com>
2019-01-28 15:33:11 +0100

    Maybe DirectX.lib is connected

---
+++ gl_testbench/D3D12Manager.cpp
@@ -0,0 +1,99 @@
+#include "D3D12Manager.h"
+
+
+void D3D12Manager::getHardwareAdapter(IDXGIFactory4 * pFactory, IDXGIAdapter1 ** ppAdapter)
+{
+	*ppAdapter = nullptr;
+
+	for (UINT adapterIndex = 0; ; ++adapterIndex) {
+		IDXGIAdapter1* pAdapter = nullptr;
+
+		if (DXGI_ERROR_NOT_FOUND == pFactory->EnumAdapters1(adapterIndex, &pAdapter)) {
+			// There are no more adapters to enumerate
+			break;
+		}
+
+
+
+		// Check to see if the adapter supports Direct3D 12, but don't create the actual 
+		// device yet.
+		if (SUCCEEDED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr))) {
+			*ppAdapter = pAdapter;
+			return;
+		}
+		pAdapter->Release();
+	}
+}
+
+void D3D12Manager::loadPipeline()
+{
+	///  -------  Enable the debug layer  -------
+	ID3D12Debug *debugController;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
+		debugController->EnableDebugLayer();
+	}
+	else {
+		throw std::exception("ERROR: Failed to getDebugInterface.");
+	}
+
+	///  -------  Create the device  -------
+	// Factory
+	IDXGIFactory4 *factory;
+	if FAILED(CreateDXGIFactory1(IID_PPV_ARGS(&factory))) {
+		throw std::exception("ERROR: Failed to create DXGIFactory1");
+	}
+	// Hardware Adapter
+	IDXGIAdapter1 *hardwareAdapter;
+	this->getHardwareAdapter(factory, &hardwareAdapter);
+	if (FAILED(D3D12CreateDevice(
+		hardwareAdapter,
+		D3D_FEATURE_LEVEL_11_0,
+		IID_PPV_ARGS(&m_device)
+	))) {
+		throw std::exception("ERROR: Failed to create Device!");
+	}
+
+	///  -------  Command Queue  -------
+	// Command Queue Description
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Flags;
+
+
+	// Fill out a command queue description, then create the command queue
+
+	// Fill out a swapchain description, then create the swap chain
+
+	// Fill out a heap description, then create a descriptor heap
+
+	// Create the render target view
+
+	// Create the command allocator
+}
+
+void D3D12Manager::loadAssets()
+{
+}
+
+D3D12Manager::D3D12Manager()
+{
+}
+
+D3D12Manager::~D3D12Manager()
+{
+}
+
+void D3D12Manager::initialize()
+{
+}
+
+void D3D12Manager::render()
+{
+}
+
+void D3D12Manager::update()
+{
+}
+
+void D3D12Manager::destroy()
+{
+}
