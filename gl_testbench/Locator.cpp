e82a6a3d771d5e81b51febda9034d527901660f1
GUSTAV BJÖRK <gubj15@student.bth.se>
2019-02-11 11:32:35 +0100

    Updated locator.

--- gl_testbench/Locator.cpp
+++ gl_testbench/Locator.cpp
@@ -1,7 +1,8 @@
 #include "Locator.h"
 
-ID3D12RootSignature* Locator::gRootSignature = nullptr;
-ID3D12Device4* Locator::gDevice = nullptr;
-IDXGISwapChain3* Locator::gSwapChain = nullptr;
-ID3D12GraphicsCommandList3* Locator::gCommandList = nullptr;
-ID3D12CommandAllocator* Locator::gCommandAllocator = nullptr;
+ID3D12RootSignature** Locator::gRootSignature = nullptr;
+ID3D12Device4** Locator::gDevice = nullptr;
+IDXGISwapChain4** Locator::gSwapChain = nullptr;
+ID3D12GraphicsCommandList3** Locator::gCommandList = nullptr;
+ID3D12CommandAllocator** Locator::gCommandAllocator = nullptr;
+ID3D12CommandQueue** Locator::gCommandQueue = nullptr;
